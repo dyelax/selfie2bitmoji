6d09b0eb6c4aa90d26516dadf2ba22c9fe2a35b5
Matt Cooper <matthew_cooper@brown.edu>
2017-11-27 17:05:17 -0500

    Upped read process count to 8

--- utils/data.py
+++ utils/data.py
@@ -114,7 +114,7 @@ def process_avatar_synth_data(df, batch_size):
     :return: A dataflow with extra processing steps applied.
     """
     df = BatchData(df, batch_size, remainder=True)
-    df = PrefetchDataZMQ(df, 8) # start processes to run the dataflow in parallel
+    df = PrefetchDataZMQ(df, 16) # start processes to run the dataflow in parallel
 
     return df
 
