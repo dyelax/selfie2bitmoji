4f8ede171eb8f9ddbb1f7cf8bb1349b8857e97ec
dyelax <matthew_cooper@brown.edu>
2017-11-29 01:45:17 -0500

    Set stdev for initializer and nr_thread for reader

--- models/avatar_synth_model.py
+++ models/avatar_synth_model.py
@@ -30,7 +30,7 @@ class AvatarSynthModel(ModelDesc):
         :param inputs: The input tensors fed in by TensorPack.
         """
         def narrow_truncated_normal_initializer(shape, dtype=None, partition_info=None):
-            initializer = tf.truncated_normal_initializer(stddev=0.1)
+            initializer = tf.truncated_normal_initializer(stddev=0.05)
             return initializer(shape, dtype=dtype, partition_info=partition_info)
 
 
