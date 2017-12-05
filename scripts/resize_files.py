4e7df7662a58b83881222c50adf806ddc8f52af3
Matt Cooper <matthew_cooper@brown.edu>
2017-12-05 00:53:57 -0500

    Test to check filesizes

--- scripts/resize_files.py
+++ scripts/resize_files.py
@@ -2,14 +2,19 @@ import os
 from glob import glob
 from imageio import imread, imwrite
 import cv2
+import numpy as np
 
 paths = glob('../data/bitmoji/*/*.png')
 
-total = len(paths)
-for i, path in enumerate(paths):
+for path in np.random.choice(paths, 10):
     img = imread(path)
-    new_img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_AREA)
-    os.remove(path)
-    imwrite(path, new_img)
+    print img.shape
 
-    print '%d / %d converted' % (i, total)
+# total = len(paths)
+# for i, path in enumerate(paths):
+#     img = imread(path)
+#     new_img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_AREA)
+#     os.remove(path)
+#     imwrite(path, new_img)
+#
+#     print '%d / %d converted' % (i, total)
