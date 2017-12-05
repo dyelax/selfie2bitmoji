3c25fb1147bd51517f66b20dcf4cac407aba8973
Matt Cooper <matthew_cooper@brown.edu>
2017-12-04 23:14:56 -0500

    model_architectures import wasn't working, so hardcoded IMG_DIMS for now

--- scripts/resize_files.py
+++ scripts/resize_files.py
@@ -2,14 +2,13 @@ import os
 from glob import glob
 from imageio import imread, imwrite
 import cv2
-from models.model_architectures import IMG_DIMS
 
 paths = glob('../data/bitmoji/*/*.png')
 
 total = len(paths)
 for i, path in enumerate(paths):
     img = imread(path)
-    new_img = cv2.resize(img, IMG_DIMS[:2], interpolation=cv2.INTER_AREA)
+    new_img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_AREA)
     os.remove(path)
     imwrite(path, new_img)
 
