21ac495ed78940662848244273b06794ae43b9c1
Matt Cooper <matthew_cooper@brown.edu>
2017-12-05 21:24:18 -0500

    Added back name scope

--- models/avatar_synth_model.py
+++ models/avatar_synth_model.py
@@ -31,51 +31,51 @@ class AvatarSynthModel(ModelDesc):
 
         :param inputs: The input tensors fed in by TensorPack.
         """
-        # with tf.variable_scope('', reuse=tf.AUTO_REUSE):
-        self.params, self.imgs = inputs
+        with tf.name_scope('Avatar_Synth'):
+            self.params, self.imgs = inputs
 
-        # Reshape params into a 1x1 'image' for convolution
-        self.preds = tf.reshape(self.params, (-1, 1, 1, BITMOJI_PARAM_SIZE))
-        for i in xrange(len(arch['conv_filters']) - 1):
-            # Apply ReLU on all but the last layer
-            activation = tf.nn.relu
-            if i == len(arch['conv_filters']) - 2:
-                activation = tf.nn.tanh
+            # Reshape params into a 1x1 'image' for convolution
+            self.preds = tf.reshape(self.params, (-1, 1, 1, BITMOJI_PARAM_SIZE))
+            for i in xrange(len(arch['conv_filters']) - 1):
+                # Apply ReLU on all but the last layer
+                activation = tf.nn.relu
+                if i == len(arch['conv_filters']) - 2:
+                    activation = tf.nn.tanh
 
-            self.preds = tf.layers.conv2d_transpose(
-                self.preds,
-                arch['conv_filters'][i + 1],
-                arch['filter_widths'][i],
-                arch['strides'][i],
-                padding=arch['padding'][i],
-                activation=tf.nn.relu,
-                name='Deconv_' + str(i),
-                reuse=tf.AUTO_REUSE,
-                trainable=False
-            )
-            self.preds = tf.layers.conv2d_transpose(
-                self.preds,
-                arch['conv_filters'][i + 1],
-                1,
-                1,
-                padding='SAME',
-                activation=activation,
-                kernel_initializer=narrow_truncated_normal_initializer,
-                bias_initializer=tf.zeros_initializer,
-                name='Conv_' + str(i),
-                reuse=tf.AUTO_REUSE,
-                trainable=False
-            )
+                self.preds = tf.layers.conv2d_transpose(
+                    self.preds,
+                    arch['conv_filters'][i + 1],
+                    arch['filter_widths'][i],
+                    arch['strides'][i],
+                    padding=arch['padding'][i],
+                    activation=tf.nn.relu,
+                    name='Deconv_' + str(i),
+                    reuse=tf.AUTO_REUSE,
+                    trainable=False
+                )
+                self.preds = tf.layers.conv2d_transpose(
+                    self.preds,
+                    arch['conv_filters'][i + 1],
+                    1,
+                    1,
+                    padding='SAME',
+                    activation=activation,
+                    kernel_initializer=narrow_truncated_normal_initializer,
+                    bias_initializer=tf.zeros_initializer,
+                    name='Conv_' + str(i),
+                    reuse=tf.AUTO_REUSE,
+                    trainable=False
+                )
 
-            # Apply batch norm on all but the last layer
-            if i < len(arch['conv_filters']) - 2:
-                self.preds = tf.layers.batch_normalization(self.preds, name='BN_' + str(i),
-                                                           reuse=tf.AUTO_REUSE)
-                self.preds = tpDropout(self.preds, keep_prob=self.args.keep_prob)
+                # Apply batch norm on all but the last layer
+                if i < len(arch['conv_filters']) - 2:
+                    self.preds = tf.layers.batch_normalization(self.preds, name='BN_' + str(i),
+                                                               reuse=tf.AUTO_REUSE)
+                    self.preds = tpDropout(self.preds, keep_prob=self.args.keep_prob)
 
-        self.cost = tf.reduce_mean(tf.square(self.imgs - self.preds), name='Cost')
+            self.cost = tf.reduce_mean(tf.square(self.imgs - self.preds), name='Cost')
 
-        self.lr = tf.Variable(self.args.lr, trainable=False, name='LR')
+            self.lr = tf.Variable(self.args.lr, trainable=False, name='LR')
 
         with tf.name_scope('Summaries'):
             pred_comp = tf.concat([self.imgs, self.preds], axis=2)
