233a203e7093fd3d01ab7111c39b138d77820f5a
Matt Cooper <matthew_cooper@brown.edu>
2017-12-14 21:16:34 -0500

    Init stddev 0.6

--- utils/s2b.py
+++ utils/s2b.py
@@ -5,5 +5,5 @@ def narrow_truncated_normal_initializer(shape, dtype=None, partition_info=None):
     A version of tf.truncated_normal_initializer with a stddev of 0.1, for use
     with tf.layers.
     """
-    initializer = tf.truncated_normal_initializer(stddev=0.05)
+    initializer = tf.truncated_normal_initializer(stddev=0.06)
     return initializer(shape, dtype=dtype, partition_info=partition_info)
